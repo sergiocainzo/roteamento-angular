bc5f046f31506fc22dcab0fe2c07b6e42d279353
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-08 16:41:28 -0300

    ajustando mais rotas
    Adição dos roteamentos:
    -redirecTo
    -pathMatch
    
    Página não encontrada ERRO 404!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PrimeiraPagina } from './primeira-pagina/primeira-pagina';
 import { SegundaPagina } from './segunda-pagina/segunda-pagina';
+import { PaginaNaoEncontrada } from './pagina-nao-encontrada/pagina-nao-encontrada';
 
 export const routes: Routes = [
-  { path: '', component: PrimeiraPagina },
+  { path: '', redirectTo: 'primeira-pagina', pathMatch: 'full' },
   { path: 'primeira-pagina', component: PrimeiraPagina },
   { path: 'segunda-pagina', component: SegundaPagina },
+  { path: '**', component:PaginaNaoEncontrada },
 ];
